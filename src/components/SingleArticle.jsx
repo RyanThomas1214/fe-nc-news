ae9ba78e4915dc7732c09095548796b782ff2231
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-12-16 12:05:24 +0000

    added non-functional voting buttons for singleArticle

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -25,7 +25,10 @@ class SingleArticle extends Component {
         <h2>{article.title}</h2>
         <h3>Written by: {article.author}</h3>
         <h5>{article.body}</h5>
-        <p>Votes: {article.votes}</p>
+        <p>
+          Votes: {article.votes} <button>Upvote</button>
+          <button>Downvote</button>
+        </p>
         <p> Comments: {article.comment_count}</p>
       </main>
     );
