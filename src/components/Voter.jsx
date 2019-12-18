598c075a1aa0cfc015eced9ff9afc98059477282
Ryan Thomas <ryan_j_thomas@hotmail.co.uk>
2019-12-18 16:25:22 +0000

    added styling to voter and cards

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -8,7 +8,7 @@ const Button = styled.button`
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid red;
-  border-radius: 3px;
+  border-radius: 25px;
   :disabled {
     background-color: red;
     color: #dddddd;
